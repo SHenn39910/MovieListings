e72558dd1495a771ce7ecfafad83b9a57f339548
Sonja Hennessy <sonjahennessy15@gmail.com>
2021-05-20 12:35:22 -0400

    put in loaders did some coding

--- movie/webpack.config.js
+++ movie/webpack.config.js
@@ -1,17 +1,22 @@
 const { Module } = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
-Module.exports = {
-    module: {
-        rules:[
-            {
-                test: "/\.js$/",
-                exclude:
-"/node_modules/",
-                use: {
-loader: '"babel-loader',
-                },
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html",
+});
 
-            },
-        ],
-    },
-}
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: "/\.js$/",
+        exclude: "/node_modules/",
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [htmlPlugin],
+};
