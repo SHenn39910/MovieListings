f12bf4ff597f23db7b50e67617fdfafe488fa103
Sonja Hennessy <sonjahennessy15@gmail.com>
2021-05-20 16:29:45 -0400

    created src/containers list.js + updated indexjs

--- movie/src/index.js
+++ movie/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import List from './containers/List';
 
 const App = () => {
-    return<h1>MovieList</h1>;
+    return<List />;
 
 };
 
