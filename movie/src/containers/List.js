9b2ab2b68d3a9057a7ecf000b1af3d434cae1260
Sonja Hennessy <sonjahennessy15@gmail.com>
2021-05-21 14:50:04 -0400

    created constructor async/await func to load data

--- movie/src/containers/List.js
+++ movie/src/containers/List.js
@@ -1,11 +1,29 @@
-import React, { Component } from 'react';
-import Card from '../components/Card/Card';
+import React, { Component } from "react";
+import Card from "../components/Card/Card";
 
-class List extends Component
-{
-    render(){
-        return <Card />;
+class List extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+      loading: true,
+    };
+    async;
+    componentDidMount();
+    {
+      const movies = await fetch("../../assets/data.json");
+      const moviesJSON = await movies.json();
+      if (moviesJSON) {
+        this.setState({
+          data: moviesJSON,
+          loading: false,
+        });
+      }
     }
-};
+  }
+  render() {
+    return <Card />;
+  }
+}
 
 export default List;
